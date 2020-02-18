9bd8c25bfb422e10e62d32451acde061921473d6
Artem Kazaryan <artem.kazaryan.tema@gmail.com>
2020-02-18 18:17:56 +0300

    add date validation function, change email validation function

--- src/utils/validation.js
+++ src/utils/validation.js
@@ -1,4 +1,31 @@
 export const validateEmail = (str) => {
   const regexp = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  return regexp.test(str);
+
+  if (!regexp.test(str)) {
+    return 'Введите email в корректном формате';
+  }
+
+  return true;
+};
+
+export const validateDate = (str) => {
+  const regexp = /^\d{2}([.-])\d{2}\1\d{4}$/;
+
+  if (!regexp.test(str)) {
+    return 'Введите дату в формате ДД-ММ-ГГГ';
+  }
+
+  const [day, month, year] = str.split('-');
+
+  if (Number(month) > 12 || Number(month) < 1) {
+    return 'Номер месяца должен быть от 1 до 12';
+  }
+
+  const daysInMonth = new Date(year, month, 0).getDate();
+
+  if (Number(day) < 1 || Number(day) > daysInMonth) {
+    return `Нет такого дня в ${month} месяце`;
+  }
+
+  return true;
 };
--- src/utils/validation.test.js
+++ src/utils/validation.test.js
@@ -3,14 +3,29 @@ import * as validation from './validation';
 const validEmails = ['email@mail.ru', 'email@gmail.com', 'petrov19@mail.ru', '123@gmail.com', 'dsa_ds@yahoo.com', 'p@mail.ru', 'ivan190@yandex.ru'];
 const invalidEmails = ['@mail.ru', 'emailgmail.com', 'petrov19@mailru', 'email@mail.', '!/email.ru', '@email@mail.ru', 'email@.ru'];
 
-describe.each(validEmails)('test email: %i', (email) => {
+describe.each(validEmails)('test emails', (email) => {
   test(`testing ${email}`, () => {
     expect(validation.validateEmail(email)).toBe(true);
   });
 });
 
-describe.each(invalidEmails)('test email: %i', (email) => {
-  test(`testing ${email}`, () => {
-    expect(validation.validateEmail(email)).toBe(false);
+describe.each(invalidEmails)('test emails', (email) => {
+  test(`testing email ${email}`, () => {
+    expect(validation.validateEmail(email)).not.toBe(true);
+  });
+});
+
+const validDates = ['12-12-2001', '03-05-1998', '12-11-1990'];
+const invalidDates = ['12/12/2001', '03-15-1998', '00-11-1990', '4-11-1990', '12-11-194'];
+
+describe.each(validDates)('test dates', (date) => {
+  test(`testing date ${date}`, () => {
+    expect(validation.validateDate(date)).toBe(true);
+  });
+});
+
+describe.each(invalidDates)('test dates', (date) => {
+  test(`testing date ${date}`, () => {
+    expect(validation.validateDate(date)).not.toBe(true);
   });
 });
