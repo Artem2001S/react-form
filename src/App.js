647afa4d665e9b77af77f14449e8df77c6048a99
Artem Kazaryan <artem.kazaryan.tema@gmail.com>
2020-02-18 11:58:13 +0300

    feat: add Input component layout

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import Input from './components/Input/Input';
 
 function App() {
   return (
-    <div />
+    <div>
+      <Input labelTitle="ФИО" name="name" placeholder="pla"/>
+      <Input labelTitle="ФИО" name="name2" />
+    </div>
   );
 }
 
