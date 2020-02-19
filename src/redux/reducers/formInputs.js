ff34ed02cdd60103b0a287beb89d7836f4a74c8e
Artem Kazaryan <artem.kazaryan.tema@gmail.com>
2020-02-19 17:54:44 +0300

    refactor formInputs reducer

--- src/redux/reducers/formInputs.js
+++ src/redux/reducers/formInputs.js
@@ -1,4 +1,10 @@
 import { CHANGE_INPUT_VALUE } from '../actions/actionTypes';
+import {
+  validatePhoneNumber,
+  validateCreditCardFormat,
+  validateDate,
+  validateEmail
+} from 'utils/validation';
 
 const initialState = [
   {
@@ -11,37 +17,42 @@ const initialState = [
     name: 'user-phone',
     label: 'Номер телефона',
     placeholder: '+7 (961) 112-31-45',
-    value: ''
+    value: '',
+    validator: validatePhoneNumber
   },
   {
     name: 'user-card',
     label: 'Номер банковской карты',
     placeholder: '0000 0000 0000 0000',
-    value: ''
+    value: '',
+    validator: validateCreditCardFormat
   },
   {
     name: 'user-date-of-birth',
     label: 'Дата рождения',
     placeholder: '01-01-1990',
-    value: ''
+    value: '',
+    validator: validateDate
   },
   {
     name: 'user-email',
     label: 'Email',
     placeholder: 'email@gmail.com',
-    value: ''
-  },
+    value: '',
+    validator: validateEmail
+  }
 ];
 
 export const formInputs = (state = initialState, action) => {
   const { payload } = action;
+
   switch (action.type) {
     case CHANGE_INPUT_VALUE:
-      return state.map(inputData => (
-        inputData.name === payload.inputName ?
-          { ...inputData, value: payload.newValue } :
-          { ...inputData }
-      ));
+      return state.map(inputData =>
+        inputData.name === payload.inputName
+          ? { ...inputData, value: payload.newValue }
+          : { ...inputData }
+      );
     default:
       return state;
   }
